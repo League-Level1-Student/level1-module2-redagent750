98cd69ae8d2f4b136590e24fd3b9a0e94b368e52
redagent750 <37556745+redagent750@users.noreply.github.com>
2019-05-07 14:23:41 -0700

    Add files via upload

---
+++ TeaMakerRunner.java
@@ -0,0 +1,11 @@
+package tea_maker;
+
+import tea_maker.Kettle.Water;
+
+public class TeaMakerRunner {
+public static void main(String[] args) {
+TeaBag bag = new TeaBag("Green");
+System.out.println(bag.getFlavor());
+
+}
+}
